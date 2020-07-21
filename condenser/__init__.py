345ac2acc834b2bfc8bddf821424df1f9e8c5acd
Sumit Kumar <31334520+sumitkumarju@users.noreply.github.com>
2020-07-21 20:42:14 +0530

    modified init

--- condenser/__init__.py
+++ condenser/__init__.py
@@ -7,5 +7,6 @@ def create_app(config_file='settings.py'):
     app.config.from_pyfile(config_file)
     app.register_blueprint(short)
     db.init_app(app)
+    app.cli.add_command(create_tables)
     return app
 
